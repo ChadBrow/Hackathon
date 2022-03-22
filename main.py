7c81ef4259a471a01bd614c5fd51375700579657
TorpidTuna <abayanzu@nd.edu>
2022-03-22 17:47:39 -0400

    adding the basics

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ pygame.init()
 
 width = 960
 height = 540
+fps = 60
 
 bgcolor = (0, 0, 0)
 fgcolor = (255, 255, 255)
@@ -14,10 +15,14 @@ window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Hackathon")
 window.fill(bgcolor)
 
+clock = pygame.time.Clock()
+
+#We should define a sprite class and all the types of stuff we have on the screen are subclasses of sprite
+
 while not ended:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             ended = 1
     pygame.display.update()
-
+    clock.tick(fps) #Set FPS
 pygame.quit()
