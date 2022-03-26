f384181686eee652cfe83b48ff6c17706b080aad
Blake C <blakecaven@outlook.com>
2022-03-25 23:34:22 -0400

    did it break

--- backend.py
+++ backend.py
@@ -1,4 +1,4 @@
-import json
+import json, random
 import classes as c
 
 """
@@ -19,12 +19,66 @@ all the big stats:
 
 
 """
+# university people
 students = c.FocusGroup()
 faculty = c.FocusGroup()
 
+# da money (donors, games, tuition)
+donors = c.FocusGroup() # approval changes % money, performance changes total amount of money
+fans = c.FocusGroup() # approval changes how many fans come out, performance changes how hype they are
+
+
+# misc pain and punishment
+"""
+Three groups of events:
+- minor
+- noteworthy
+- severe
+
+or should we do
+- good
+- neutral 
+- bad
+"""
+scriptedEvents = [] # array of c.Event() objects
+randomEvents = [[], [], []] # array of c.Event() -> impacts the university
+econEvents = [[], [], []] # array of events that happen to the economy (impacting the fans and donors)
+
+# constants
+tuition = 1000 # just a random constant.
+eventChance = 50 # number between 1 and 100
 
 mainStats = {
     "performance": 0,
     "approval": 0,
     "budget": 0
 }
+def startup(dificulty):
+    if dificulty == "medium":
+        # university people
+        students = c.FocusGroup(0.5, 0.04, 0.5, 0.04)
+        faculty = c.FocusGroup(0.5, 0.04, 0.5, 0.04)
+
+        # da money (donors, games, tuition)
+        donors = c.FocusGroup(0.5, 0.04, 0.5, 0.04) # approval changes % money, performance changes total amount of money
+        fans = c.FocusGroup(0.5, 0.04, 0.5, 0.04) # approval changes how many fans come out, performance changes how hype they are
+
+def getEvents(fileName):
+    text = ""
+    with open(fileName, 'r') as f:
+        text += "".join(f.readlines)
+    print(text)    
+    jsonData = json.loads(text)
+    
+
+def tick():
+
+    # check for events
+    changeOfEvent = random.randint(1, 100)
+
+
+    # update the people
+    students.update()
+    faculty.update()
+    donors.update()
+    fans.update()
--- classes.py
+++ classes.py
@@ -1,8 +1,9 @@
-class FocusGroup:
-
-    def __innit__(self, startApproval = 0, startPerform = 0):
+class FocusGroup: # are we just calling this the money too (where "performance" is "willingness to donate")
+    def __init__(self, startApproval = 0, startDApproval = 0, startPerform = 0, startDPerformance = 0):
         self.approval = startApproval
+        self.dApproval = startDApproval
         self.performance = startPerform
+        self.dPerformance = startDPerformance
     
     def modApproval(self, var):
         self.approval += var
@@ -10,7 +11,18 @@ class FocusGroup:
     def modPerformance(self, var):
         self.performance += var
     
+    def modDApproval(self, var):
+        self.dApproval += var
     
+    def modDPerformance(self, var):
+        self.dPerformance += var
+    
+    def update(self):
+        self.approval += self.dApproval
+        self.performance += self.dPerformance
+        self.dApproval = 0.75 * self.dApproval
+        self.dPerformance = 0.75 * self.dPerformance
+
     def returnDict(self):
         return {"approval" : self.approval, "performance" : self.approval}
 
@@ -23,10 +35,19 @@ class Choice:
     
 
 class Event:
-    def __innit__(self, title, image, body, choices):
+    def __init__(self, title, image, body, choices):
         self.title = title
         self.image = image
         self.body = body
         self.choices = choices
+
+class MoneySink:
+    def __init__(self, effects, funding = 0, maxFunding = 0): 
+        self.funding = funding
+        self.maxFunding = maxFunding
+        self.effects = effects #list containing lists. These contained lists contain effect function and multiplier
     
-    
+    def modFunding(self, var):
+        self.funding += var
+        for effect in self.effects:
+            effect[0](effect[1] * var)
