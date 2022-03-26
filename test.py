f9326c25d79630e43153a2fd0e8773a06dc6ba60
ChadBrow <chad.j.brown.03@gmail.com>
2022-03-26 11:13:40 -0400

    aaaa

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
+import test2.py as t
+# from data import FOCUS_GROUPS
 
-from data import FOCUS_GROUPS
+# for i in FOCUS_GROUPS:
+#     print(FOCUS_GROUPS[i].performance)
+# for i in FOCUS_GROUPS:
+#     FOCUS_GROUPS[i].update()
 
-for i in FOCUS_GROUPS:
-    print(FOCUS_GROUPS[i].performance)
-for i in FOCUS_GROUPS:
-    FOCUS_GROUPS[i].update()
+# for i in FOCUS_GROUPS:
+#     print(FOCUS_GROUPS[i].performance)
 
-for i in FOCUS_GROUPS:
-    print(FOCUS_GROUPS[i].performance)
+t.do[0]([t.do[1]])
+
+def hi(num):
+    print("Hi", num)
