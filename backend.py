54cb669eeb1166eb43293d5c5f4f5490ecc6d020
BCaven <blakecaven@outlook.com>
2022-03-26 12:52:44 -0400

    chad brow

--- backend.py
+++ backend.py
@@ -5,12 +5,14 @@ import json, random
 from data import * # import all the variables from data.py
 
 
-# constants
+# global variables
 tuition = 1000 # just a random constant.
 eventChance = 50 # number between 1 and 100
 savings = 1000 # total savings
 recommendedBudget = 1000 # how much you can spend without dipping into your savings
 spentAmmount = 0
+chosenEvent = None
+academicLevel = 0
 
 mainStats = {
     "performance": 0,
@@ -36,6 +38,24 @@ def guiChoice():
 def tick():
 
     # Update FOCUS_GROUPS
+    #Donor approval target is equal to the academic level
+    academicLevel = (FOCUS_GROUPS["students"].performance + FOCUS_GROUPS["faculty"].performance) / 2
+    FOCUS_GROUPS["donors"].modApprovalTarget(academicLevel - FOCUS_GROUPS["students"].performanceTarget) 
+    FOCUS_GROUPS["donors"].updateApproval()
+
+    #fan approval target is equal to campus happiness
+
+    #student and faculty performance wanna go to student and faculty approval
+    #update student and faculty approval then set student and faculty performance target equal to new approval then update performance
+    FOCUS_GROUPS["students"].updateApproval()
+    FOCUS_GROUPS["faculty"].updateApproval()
+
+    FOCUS_GROUPS["students"].modPerformanceTarget(FOCUS_GROUPS["students"].approval - FOCUS_GROUPS["students"].performanceTarget)
+    FOCUS_GROUPS["faculty"].modPerformanceTarget(FOCUS_GROUPS["faculty"].approval - FOCUS_GROUPS["faculty"].performanceTarget)
+
+    FOCUS_GROUPS["students"].updatePerformance()
+    FOCUS_GROUPS["faculty"].updatePerformance()
+
     for c in FOCUS_GROUPS:
         FOCUS_GROUPS[c].update()
 
@@ -45,9 +65,9 @@ def tick():
     if chanceOfEvent >= eventChance:
         e = random.randint(0, len(RANDOM_EVENTS)-1)
         event = RANDOM_EVENTS[e] # pass this event to anar's front-end
-        pushEvent(event) # placeholder function for passing the event to anar
+        chosenEvent = pushEvent(event) # placeholder function for passing the event to anar
     choice = guiChoice() # this is a placeholder for the choice passed back by anar's gui
-    enactEvent(event, choice) # this is a placeholder function for however we want to do this
+    enactEvent(chosenEvent, choice) # this is a placeholder function for however we want to do this
 
     # update budget
     
