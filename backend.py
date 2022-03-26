acb244d2197e3716ad556fb7c26eddd0f77d7e00
BCaven <blakecaven@outlook.com>
2022-03-26 02:35:23 -0400

    jack-san

--- backend.py
+++ backend.py
@@ -1,3 +1,4 @@
+
 import json, random
 
 # import the data
@@ -16,22 +17,37 @@ mainStats = {
     "approval": 0,
     "budget": 0
 }
-def revenue():
+def revenue(event, ):
+
     return 0 # make this something useful
+def pushEvent(event):
+    # I dont know how to do this right now, but we need to send this event to anar
+    # maybe just have him check for the current event, but still want the placeholder
+    pass
+def guiChoice():
+    return 0
+
 def tick():
 
     # Update FOCUS_GROUPS
     for c in FOCUS_GROUPS:
         FOCUS_GROUPS[c].update()
+
+    # check for events
+    chanceOfEvent = random.randint(1, 100)
+    if chanceOfEvent >= eventChance:
+        e = random.randint(0, len(RANDOM_EVENTS)-1)
+        event = RANDOM_EVENTS[e] # pass this event to anar's front-end
+        pushEvent(event)
+    choice = guiChoice()
     
+
     # update budget
     
     savings = savings - spentAmmount + revenue()
 
 
 
-    # check for events
-    changeOfEvent = random.randint(1, 100)
 
 
 
